a9bf1438c3a8001d3c1fccabbbe25397280e9137
Asassuun <136801990+Asassuun@users.noreply.github.com>
2023-06-16 12:37:25 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+def sort_numbers(numbers):
+    sorted_numbers = sorted(numbers)
+    return sorted_numbers
+
+# Пример использования функции
+unsorted_list = [9, 3, 7, 1, 5]
+sorted_list = sort_numbers(unsorted_list)
+print("Отсортированный список:", sorted_list)
